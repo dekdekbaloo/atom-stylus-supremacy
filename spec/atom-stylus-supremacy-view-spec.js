13d474ebba0dc56e29a754930988e308ee7f4ebc
Jessada Trirongkit <jessada.trirongkit@gmail.com>
2017-04-28 19:17:26 +0700

    using supremacy worked!

---
+++ spec/atom-stylus-supremacy-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomStylusSupremacyView from '../lib/atom-stylus-supremacy-view';
+
+describe('AtomStylusSupremacyView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
